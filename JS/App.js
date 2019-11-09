8f47492c33123b68d8e87408d364e30e5fc367c1
Jake <lejake888@gmail.com>
2019-11-09 23:18:22 +0000

    Adding key presses

--- JS/App.js
+++ JS/App.js
@@ -8,6 +8,15 @@ let ball = {
     top: 505
 }
 
+let blocks =
+    [ 
+        {left: 300, top: 100},
+        {left: 350, top: 100},
+        {left: 400, top: 100},
+        {left: 450, top: 100},
+        {left: 500, top: 100}
+    ]
+
 document.onkeydown = function (e) {
         if (e.keyCode === 37){
             player.left = player.left - 15
@@ -16,11 +25,20 @@ document.onkeydown = function (e) {
         }
         else if(e.keyCode === 39){
             player.left = player.left + 15
-            console.log("left")
+            console.log("right")
             movePlayer()
         }
 }
 
 let movePlayer = () => {
     document.getElementById("player").style.left = player.left + "px"
- }
+}
+
+let createBoxes = () => { 
+    document.getElementById("blocks").innerHTML = "";
+    for (let i = 0; i < blocks.length; i++) {
+        document.getElementById("blocks").innerHTML +=`<div id='blocks' style='left:${blocks[i].left}px; top:${blocks[i].top}px;'></div>`; 
+    }
+}
+
+createBoxes()
