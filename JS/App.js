b6bcfd60176efc9bd0c5c7815ff8a393cb2df976
Jake <lejake888@gmail.com>
2019-11-09 21:12:17 +0000

    Not using canvas, converting to CSS

--- JS/App.js
+++ JS/App.js
@@ -1,41 +1,26 @@
-// let player = {
-//     top: 720,
-//     left: 680
-// }
+let player = {
+    left: 600,
+    top: 520
+}
 
-// let ball = {
-//     top: 700,
-//     left: 680
-// }
+let ball = {
+    left: 630,
+    top: 505
+}
 
-// document.onkeydown = function (e) {
-//         if (e.keyCode === 37){
-//             player.left = player.left - 15
-//             console.log("left")
-//             movePlayer()
-//         }
-//         else if(e.keyCode === 39){
-//             player.left = player.left + 15
-//             console.log("left")
-//             movePlayer()
-//         }
-// }
+document.onkeydown = function (e) {
+        if (e.keyCode === 37){
+            player.left = player.left - 15
+            console.log("left")
+            movePlayer()
+        }
+        else if(e.keyCode === 39){
+            player.left = player.left + 15
+            console.log("left")
+            movePlayer()
+        }
+}
 
-// let movePlayer = () => {
-//     document.getElementById("player").style.left = player.left + "px"
-//  }
-
-var canvas = document.getElementById("myCanvas");
-var shape = canvas.getContext("2d");
-
-shape.beginPath();
-shape.rect(215, 280, 50, 10);
-shape.fillStyle = "white";
-shape.fill();
-shape.closePath();
-
-shape.beginPath();
-shape.arc(240, 276, 4, 0, Math.PI*2, false);
-shape.fillStyle = "white";
-shape.fill();
-shape.closePath();
+let movePlayer = () => {
+    document.getElementById("player").style.left = player.left + "px"
+ }
